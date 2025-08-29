2e4d0595130f731f8e5cb045b1865adeaf67ba40
Subhrajit Mohapatra <subhrajit2021@gmail.com>
2025-08-29 03:17:16 -0700

    Updated App.js

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [
-        React.createElement("div", { id: "child1" }, [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag"),
-            React.createElement("h3", {}, "I am h3 tag"),
-        ]),
-
-        React.createElement("div", { id: "child2" }, [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag"),
-            React.createElement("h3", {}, "I am h3 tag"),
-        ])
-    ]
-);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+// HEADER
+// logo
+// nav itenms
+// BODY
+// search
+// restaurant conatainer - restaurant cards
+// Footer
+// copyright
+// links
+// Address
+// contact
+// information
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
